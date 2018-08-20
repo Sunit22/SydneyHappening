58ac35703de1c417131c0bf992b2f8a72cfdd170
Sunit Singh <sunitsingh2291@gmail.com>
2018-08-20 17:54:10 +1000

    added events component and routing modules

--- SydneyHappening/src/app/app.module.ts
+++ SydneyHappening/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventsComponent } from './events/events.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent}, 
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'events', component: EventsComponent }  
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    EventsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
